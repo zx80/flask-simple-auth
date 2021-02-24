d708d57599311926a0782dd4b39946ef6abc58f1
Fabien Coelho <fabien@coelho.net>
2021-02-24 20:39:43 +0100

    swith to setup.cfg & a small setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-from setuptools import setup, find_packages
+# this file seems necessary for "pip install -e ."
+import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(
-    name="FlaskSimpleAuth",
-    version="1.4.0",
-    packages=find_packages(),
-    author="Fabien Coelho",
-    author_email="flask.auth@coelho.net",
-    url="https://github.com/zx80/flask-simple-auth",
-    install_requires=["passlib"],
-    description="Simple authentication for Flask, emphasizing configurability",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
-        "Programming Language :: Python",
-        "Environment :: Web Environment",
-        "Framework :: Flask",
-        "Topic :: Software Development :: Libraries :: Python Modules"
-    ]
-)
+setuptools.setup()
