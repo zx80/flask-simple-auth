18ac1be56c2cdd3db260a9d9c91a03a43b9359a8
Fabien Coelho <fabien@coelho.net>
2021-06-06 15:08:50 +0200

    simpler code

--- demo/database.py
+++ demo/database.py
@@ -2,7 +2,7 @@
 # DATABASE CONNECTION AND QUERIES
 #
 
-import anodb  # type: ignore
+from anodb import DB  # type: ignore
 from FlaskSimpleAuth import Reference, Flask, Response
 db = Reference()
 
@@ -12,5 +12,5 @@ def db_commit(res: Response):
 
 def init_app(app: Flask):
     conf = app.config
-    db.set(anodb.DB(conf["DB_TYPE"], conf["DB_CONN"], conf["DB_SQL"], conf["DB_OPTIONS"]))
+    db.set(DB(conf["DB_TYPE"], conf["DB_CONN"], conf["DB_SQL"], conf["DB_OPTIONS"]))
     app.after_request(db_commit)
