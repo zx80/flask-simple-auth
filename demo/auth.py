108876fb7674107875b7f0b6bd8390b346eaa42e
Fabien Coelho <fabien@coelho.net>
2021-06-11 23:04:50 +0200

    add more typing

--- demo/auth.py
+++ demo/auth.py
@@ -2,14 +2,15 @@
 # AUTHENTICATION AND AUTHORIZATION HOOKS
 #
 
+from typing import Optional
 from FlaskSimpleAuth import Flask
 from database import db
 
-def get_user_pass(login: str):
+def get_user_pass(login: str) -> Optional[str]:
     res = db.get_user_data(login=login)
     return res[1] if res else None
 
-def user_in_group(login: str, group: str):
+def user_in_group(login: str, group: str) -> bool:
     res = db.get_user_data(login=login)
     return bool(res[2]) if res and group == "ADMIN" else res is not None
 
