c3d83197d0acd65e5f9b5d77631b039e6addc01c
Fabien Coelho <fabien@coelho.net>
2022-06-18 19:31:59 +0200

    fix tests

--- test/test_flask.py
+++ test/test_flask.py
@@ -692,13 +692,13 @@ def test_reference():
         r = fsa.Reference(obj="hello", fun=gen_data)
         assert False, "should have raised an exception"
     except Exception as e:
-        assert "reference cannot set both obj and fun" in str(e)
+        assert "Reference cannot set both obj and fun" in str(e)
     try:
         r = fsa.Reference(set_name="add")
         r.add()
         assert False, "missing parameter in previous call"
     except Exception as e:
-        assert "reference must set either obj or fun" in str(e)
+        assert "Reference must set either obj or fun" in str(e)
     # auto thread
     r = fsa.Reference()
     r._set_fun(lambda i: i)
