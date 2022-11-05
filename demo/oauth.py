908c2af314e885a7378e796d84ee0272be742419
Fabien Coelho <fabien@coelho.net>
2022-11-05 14:43:53 +0100

    add preliminary oauth example

---
+++ demo/oauth.py
@@ -0,0 +1,29 @@
+#
+# JWT authorization
+#
+
+from FlaskSimpleAuth import Blueprint, jsonify as json, current_app as app
+from database import db
+
+oauth = Blueprint("oauth", __name__)
+
+
+# GET /oauth
+@oauth.get("/oauth", authorize="read", auth="token")
+def get_oauth():
+    return json(db.get_user_data(login=app.get_user())), 200
+
+
+# PATCH /oauth (email): update user email
+@oauth.patch("/oauth", authorize="write", auth="token")
+def patch_oauth(email: str):
+    db.upd_user_email(login=app.get_user(), email=email)
+    return "", 204
+
+
+# DELETE /oauth: unregister
+@oauth.delete("/oauth", authorize="delete", auth="token")
+def delete_oauth():
+    db.del_user_login(login=app.get_user())
+    app.clear_caches()  # BAD, should wait for cache expiration
+    return "", 204
