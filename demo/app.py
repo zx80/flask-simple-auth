29a766b145fe24edac21f9a8c4c947aabdfbf767
Fabien Coelho <fabien@coelho.net>
2023-01-29 19:27:22 +0100

    use a full random uuid

--- demo/app.py
+++ demo/app.py
@@ -44,7 +44,7 @@ def get_who():
 # POST /upload (file): upload a file!
 @app.post("/upload", authorize="ALL")
 def post_upload(file: fsa.FileStorage):
-    filename = str(uuid.uuid1()) + ".tmp"
+    filename = str(uuid.uuid4()) + ".tmp"
     file.save(app.config["APP_UPLOAD_DIR"] + "/" + filename)
     return f"{file.filename} saved as {filename}", 201
 
