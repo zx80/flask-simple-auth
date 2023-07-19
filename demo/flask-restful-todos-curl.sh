2718fd3dc1064e74018dffd602ef3624ccec49a4
Fabien Coelho <fabien@coelho.net>
2023-07-19 17:14:25 +0200

    add echo to avoid mixing up responses

--- demo/flask-restful-todos-curl.sh
+++ demo/flask-restful-todos-curl.sh
@@ -4,17 +4,24 @@
 #
 # NOTE fails with todos-frf because "task" is expected in json
 
-req="curl -si -u calvin:sesame -X"
 URL="http://0.0.0.0:5000"
 
+function req()
+{
+  curl -si -u calvin:sesame -X "$@"
+  echo
+}
+
+
+
 # check for errors
-curl -si -X GET $URL/todos/todo1            # 401
-$req DELETE $URL/todos/todo0               # 404
-$req POST -d stuff="WIP 0" $URL/todos      # 400
+curl -si -X GET $URL/todos/todo1          # 401
+req DELETE $URL/todos/todo0               # 404
+req POST -d stuff="WIP 0" $URL/todos      # 400
 
 # exercise all methods and path
-$req POST -d task="WIP 1" $URL/todos       # 201
-$req GET $URL/todos/todo1                  # 200
-$req DELETE $URL/todos/todo1               # 204
-$req PUT -d task="WIP 2" $URL/todos/todo2  # 204
-$req GET $URL/todos                        # 200
+req POST -d task="WIP 1" $URL/todos       # 201
+req GET $URL/todos/todo1                  # 200
+req DELETE $URL/todos/todo1               # 204
+req PUT -d task="WIP 2" $URL/todos/todo2  # 204
+req GET $URL/todos                        # 200
