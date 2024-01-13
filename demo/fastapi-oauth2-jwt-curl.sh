349ca61a65524d13dafc078621aec1c8802b8491
Fabien Coelho <fabien@coelho.net>
2024-01-13 17:32:33 +0100

    simpler script

--- demo/fastapi-oauth2-jwt-curl.sh
+++ demo/fastapi-oauth2-jwt-curl.sh
@@ -3,8 +3,8 @@
 URL=http://localhost:5000
 
 jwt_token=$(
-  curl -si -X POST -d username=johndoe -d password=secret $URL/token |
-  tee /dev/tty | tail -4 | jq .access_token | tr -d '"')
+  curl -s -X POST -d username=johndoe -d password=secret $URL/token |
+  tee /dev/tty | jq .access_token | tr -d '"')
 
 echo "# JWT Token: $jwt_token"
 
